2319ce0439868b6d32c5bcffddaea70f717ce505
aman tyagi <amantyagi7024@gmail.com>
2024-11-28 12:06:21 +0530

    new

---
+++ script.js
@@ -0,0 +1,28 @@
+alert("aman tyagi");
+// variables
+// var , let ,const (three ways to define variables)
+// alert , console , document.write ye display karane ke liye hote hai 
+// var name= "aman tyagi";
+// document.write(name);
+// console.log(name);
+// console.log(typeof(name));
+// name ko '' code or "" code me likh sakte hai 
+// variable ka datatyape pata karne ke liye 
+var age = 20;
+console.log(age);
+console.log(typeof(age))
+var isEligible = true;
+console.log(typeof(isEligible))
+var data="null"
+console.log(typeof(data))
+// rules for writing variable name
+// 1. should not start with a number or underscore
+// 2. can be written in pascal style or camel style 
+// 3. underscor or dollar signs  should not be used at start of variable name
+// camel case= first later small and secend is capital like that isEligible
+var name="wwwwwwwww";
+var age=40;
+var city="sehore";
+console.log(name+" "+ age);
+console.log(typeof(name+" "+ age));
+document.write(name+" "+ age);
